49fd0105665e2e38ef979ede6b63132855dc4ad5
tetrau <tetrau01@gmail.com>
2019-05-14 09:42:56 +0800

    Using ObjectURL to implement Export to Json

--- src/app-config.js
+++ src/app-config.js
@@ -4,6 +4,13 @@ class AppConfig extends Component {
     constructor(props) {
         super(props);
         this.clearDatabase = this.clearDatabase.bind(this);
+        let exportHabits = []
+        for (let habitId in this.props.habits) {
+            exportHabits.push(this.props.habits[habitId])
+        }
+        exportHabits = JSON.stringify(exportHabits, null, 2);
+        exportHabits = new Blob([exportHabits], {type : 'application/json'});
+        this.exportHabitsURL = URL.createObjectURL(exportHabits);
     }
 
     async clearDatabase() {
@@ -13,13 +20,11 @@ class AppConfig extends Component {
             this.props.clearHabit()
         }
     }
-
+    componentWillUnmount(){
+        URL.revokeObjectURL(this.exportHabitsURL);
+    }
     render() {
-        let exportHabits = []
-        for (let habitId in this.props.habits) {
-            exportHabits.push(this.props.habits[habitId])
-        }
-        exportHabits = JSON.stringify(exportHabits, null, 2);
+        
         return (
             <React.Fragment>
                 <div className="row">
@@ -29,7 +34,7 @@ class AppConfig extends Component {
                 </div>
                 <div className="row">
                     <div className="col">
-                        <a className="btn btn-outline-primary btn-lg btn-block mt-2" download="habit.json" href={'data:text/plain;charset=utf-8,' + encodeURIComponent(exportHabits)}>Export to Json</a>
+                        <a className="btn btn-outline-primary btn-lg btn-block mt-2" download="habit.json" href={this.exportHabitsURL}>Export to Json</a>
                     </div>
                 </div>
             </React.Fragment>
